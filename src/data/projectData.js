c534faa29bec10d57e014e14c0a71ebd7e405a8f
Wulan <widyawulangit19@gmail.com>
2026-03-15 23:59:26 +0700

    perubahan

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,12 +1,15 @@
-// import client1 from '../assets/client_1.png'
-// import client2 from '../assets/client_2.png'
-// import client3 from '../assets/client_3.png'
-// import client4 from '../assets/client_4.png'
-// import client5 from '../assets/client_5.png'
-// import client6 from '../assets/client_6.png'
-// import client7 from '../assets/client_7.png'
-// import client8 from '../assets/client_8.png'
-import client from '../assets/btiPT.png'
+import client1 from '../assets/project1.jpg'
+import client2 from '../assets/project2.png'
+import client3 from '../assets/project3.png'
+import client4 from '../assets/project4.png'
+import client5 from '../assets/project5.png'
+import client6 from '../assets/project6.png'
+import client7 from '../assets/client_7.png'
+import client8 from '../assets/project8.png'
+import client9 from '../assets/project9.png'
+import client10 from '../assets/project10.png'
+import client11 from '../assets/project11.png'
+// import client from '../assets/btiPT.png'
 
 export const projectData = [
     {
@@ -14,67 +17,67 @@ export const projectData = [
         title: "Patimban Port Development",
         description:
             "Renovation Eks Site Office Engineering & Construction Patimban Project",
-        img: client
+        img: client1
     },
     {
         id: 2,
         title: "CHR AXIA 3",
         description:
             "Fire Hydrant & Sprinkler System Works Pump Room & 1st Floor AXIA 3 North Tower",
-        img: client
+        img: client2
     },
     {
         id: 3,
         title: "CHR AXIA 3",
         description: "Electrical & Electronic System 8th Floor",
-        img: client
+        img: client3
     },
     {
         id: 4,
         title: "PT. Insera Sena",
         description: "Mechanical & Electrical System ASRS Phase 2",
-        img: client
+        img: client4
     },
     {
         id: 5,
         title: "PT. Kawanishi Warehouse Indonesia",
         description: "Installation Conveyor Kawanishi Project",
-        img: client
+        img: client5
     },
     {
         id: 6,
         title: "PT. Softex Indonesia",
         description: "Installation Conveyor Softex Project",
-        img: client
+        img: client6
     },
     {
         id: 7,
         title: "PT. Hon Chuan Indonesia",
         description: "Installation and Curtain Procurement",
-        img: client
+        img: client7
     },
     {
         id: 8,
         title: "PT. Tirta Kencana Cahaya Mandiri",
         description: "Installation of a 1000 liter diesel tank",
-        img: client
+        img: client8
     },
     {
         id: 9,
         title: "PT. Citra Mahasurya Industries",
         description: "Cable Drawing Machine Commissioning",
-        img: client
+        img: client9
     },
     {
         id: 10,
         title: "PT. Moya Bekasi Jaya",
         description: "Cable pulling and dismantling of Cremona",
-        img: client
+        img: client10
     },
     {
         id: 11,
         title: "PT. Bagava Alam Semesta",
         description: "Making Pump Control Panel",
-        img: client
+        img: client11
     },
 ];
